b9732762e02304fde0de81b99f13c74568dc058a
dowon938 <68101878+dowon938@users.noreply.github.com>
2020-12-23 10:36:04 +0900

    promise assignment fin

--- callback-to-promise.js
+++ callback-to-promise.js
@@ -30,13 +30,29 @@ const userStorage = new UserStorage();
 const id = prompt("input your id");
 const password = prompt("input your password");
 
-userStorage
-  .loginUser(id, password)
-  .then(userStorage.getRoles)
-  .then((user) => alert(`Hello ${user.name},${user.role} login success!!`))
-  .catch(console.log);
+// userStorage
+//   .loginUser(id, password)
+//   .then(userStorage.getRoles)
+//   .then((user) => alert(`Hello ${user.name},${user.role} login success!!`))
+//   .catch(console.log);
 
-async function lognIn() {
-  const loginUser = await loginUser(id, password);
-  const getRoles = await getRoles(id);
+// async function lognIn() {
+//   const loginUser = await userStorage.loginUser(id, password);
+//   const user = await userStorage.getRoles(loginUser);
+//   return `Hello ${user.name},you have a ${user.role} role!`;
+// }
+// lognIn() //
+//   .then(alert)
+//   .catch(console.log);
+
+async function lognInTryCatch() {
+  try {
+    const loginUser = await userStorage.loginUser(id, password);
+    const user = await userStorage.getRoles(loginUser);
+    return `Hello ${user.name},you have a ${user.role} role!`;
+  } catch (error) {
+    return error;
+  }
 }
+
+lognInTryCatch().then(alert);
